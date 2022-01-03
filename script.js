a881fd8a7842fa3d5805a51e7f9f1d65cce5f9b7
Victor <victorari856@gmail.com>
2022-01-03 15:45:48 -0300

    Portfolio

---
+++ script.js
@@ -0,0 +1,23 @@
+/*Botão menu hamburguer*/
+
+const btnHamburguer = document.getElementById('btn-hamburguer');
+//Pegou o elemento pelo id(no caso o button btn-hamburguer)
+
+function toggleMenu(event){ //Função que vai executar a ação do toggle
+ if (event.type === 'touchstart') event.preventDefault();/*Ou seja,
+ se o evento for "touch", colocamos para que não ative nada além
+ do padrão(preventDefault), que nesse caso seria o "touch" */ 
+
+const header = document.getElementById('header'); 
+//Div de navegação(about)
+  header.classList.toggle('active'); /*(Adicione caso tenha, remova caso nn tenha)
+  Essa é a fuunção do toggle nesse caso*/
+  const active = header.classList.contains('active'); /*Ou seja,
+  caso for active é true, caso contrário, é false.*/ 
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) 
+  event.currentTarget.setAttribute('area-label', 'Fechar Menu');
+  /*Ou seja, assim que abrir o menu, vai ler a mensagem de fechar menu*/ 
+  event.currentTarget.setAttribute('area-label', 'Abrir Menu');
+}
+
