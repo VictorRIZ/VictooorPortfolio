cf200c375c17d5c427aab046f371c614bbcbe455
Victor <victorari856@gmail.com>
2022-08-02 15:53:06 -0300

    Reconstrução do portfólio

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
-/*Botão menu hamburguer*/
-
-const btnHamburguer = document.getElementById('btn-hamburguer');
-//Pegou o elemento pelo id(no caso o button btn-hamburguer)
-
-function toggleMenu(event){ //Função que vai executar a ação do toggle
- if (event.type === 'touchstart') event.preventDefault();/*Ou seja,
- se o evento for "touch", colocamos para que não ative nada além
- do padrão(preventDefault), que nesse caso seria o "touch" */ 
-
-const abouts = document.getElementById('abouts'); 
-//Div de navegação(abouts)
-  abouts.classList.toggle('active'); /*(Adicione caso tenha, remova caso nn tenha)
-  Essa é a fuunção do toggle nesse caso*/
-  const active = abouts.classList.contains('active'); /*Ou seja,
-  caso for active é true, caso contrário, é false.*/ 
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) 
-  event.currentTarget.setAttribute('area-label', 'Fechar Menu');
-  /*Ou seja, assim que abrir o menu, vai ler a mensagem de fechar menu*/ 
-  event.currentTarget.setAttribute('area-label', 'Abrir Menu');
-}
-
-
-btnHamburguer.addEventListener('click', toggleMenu);
-//Ele vai observar o evento, no caso o evento de click
-// Código destinado para o menu hamburguer
-
-btnHamburguer.addEventListener('touchstart', toggleMenu);
-/*Se não corrigir, ele vai aplicar o efeito de "click" duas vezes, 
-ou seja, abre e fecha, impossibilitando assim de abrir o menu*/ 
-
-
-function toggleMenu(){
-  var aboutsToggle = document.getElementById('abouts');
-  var contentMenu = document.querySelector('.content-menu');
-  aboutsToggle.classList.toggle('active');
-  contentMenu.classList.toggle('active');
-}
-
 alert("For a better experience, use 80% to resolution. / Para uma melhor experiência, utilize a resolução em 80%.");
 
+window.addEventListener('scroll', OnScroll)
+
+OnScroll()
+//Function destinada a tudo relacionado ao scroll da página
+function OnScroll(){
+    showNavOnScroll()
+    showReturnToTopOnScroll()
+}
+
+function showNavOnScroll(){
+    var navigation = document.getElementById("navigation");
+    if (scrollY > 30) {//Se for maior que 0, ative.
+        navigation.classList.add('scroll')//Adicionar classe via JS
+        //Lembrando que já estilizei essa classe no CSS
+        } else { //senão, remova.
+            navigation.classList.remove('scroll')
+        }
+}
+
+function showReturnToTopOnScroll(){
+    var returnToTop = document.getElementById("returnToTop");
+    if (scrollY > 400) {
+        returnToTop.classList.add('show')
+        } else { 
+            returnToTop.classList.remove('show')
+        }
+}
